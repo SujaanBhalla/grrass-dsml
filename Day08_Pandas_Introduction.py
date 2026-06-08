84f368571f62476fa7c44e248b931fa597570094
SujaanBhalla <bhallasujaan@gmail.com>
2026-06-08 10:58:03 +0530

    Added file1.csv dataset

---
+++ Day08_Pandas_Introduction.py
@@ -0,0 +1,42 @@
+# import pandas as pd
+
+# data = {
+#     "name": ["dheeraj", "kunal", "praveen", "anjali", "abhishek singh"],
+
+#     "age": [20, 21, 22, 23, 24],
+
+#     "salary": [
+#         {"In-hand": 15000, "ctc": "1.2lpa"},
+#         {"In-hand": 20000, "ctc": "2.2lpa"},
+#         {"In-hand": 25000, "ctc": "3.2lpa"},
+#         {"In-hand": 30000, "ctc": "3.2lpa"},
+#         {"In-hand": 40000, "ctc": "4.2lpa"}
+#     ]
+# }
+
+# df = pd.DataFrame(data)
+
+# print(df)
+
+
+
+import pandas as pd
+
+data = {
+    "name": ["dheeraj", "kunal", "praveen", "anjali", "abhishek singh"],
+
+    "age": [20, 21],
+
+    "salary": [
+        {"In-hand": 15000, "ctc": "1.2lpa"},
+        {"In-hand": 20000, "ctc": "2.2lpa"},
+        {"In-hand": 25000, "ctc": "3.2lpa"},
+        {"In-hand": 30000, "ctc": "3.2lpa"},
+        {"In-hand": 40000, "ctc": "4.2lpa"}
+    ]
+}
+
+df = pd.DataFrame(data)
+
+print(df)
+
