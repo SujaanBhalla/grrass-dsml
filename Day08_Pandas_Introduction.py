f8aabffe069ffa38efac18987b14e49955094b7a
SujaanBhalla <bhallasujaan@gmail.com>
2026-06-08 22:26:06 +0530

    Added Excel dataset

--- Day08_Pandas_Introduction.py
+++ Day08_Pandas_Introduction.py
@@ -1,22 +1,22 @@
-# import pandas as pd
+import pandas as pd
 
-# data = {
-#     "name": ["dheeraj", "kunal", "praveen", "anjali", "abhishek singh"],
+data = {
+    "name": ["dheeraj", "kunal", "praveen", "anjali", "abhishek singh"],
 
-#     "age": [20, 21, 22, 23, 24],
+    "age": [20, 21, 22, 23, 24],
 
-#     "salary": [
-#         {"In-hand": 15000, "ctc": "1.2lpa"},
-#         {"In-hand": 20000, "ctc": "2.2lpa"},
-#         {"In-hand": 25000, "ctc": "3.2lpa"},
-#         {"In-hand": 30000, "ctc": "3.2lpa"},
-#         {"In-hand": 40000, "ctc": "4.2lpa"}
-#     ]
-# }
+    "salary": [
+        {"In-hand": 15000, "ctc": "1.2lpa"},
+        {"In-hand": 20000, "ctc": "2.2lpa"},
+        {"In-hand": 25000, "ctc": "3.2lpa"},
+        {"In-hand": 30000, "ctc": "3.2lpa"},
+        {"In-hand": 40000, "ctc": "4.2lpa"}
+    ]
+}
 
-# df = pd.DataFrame(data)
+df = pd.DataFrame(data)
 
-# print(df)
+print(df)
 
 
 
@@ -37,6 +37,14 @@ data = {
 }
 
 df = pd.DataFrame(data)
+
+# 
+
+import pandas as pd
+
+url = "https://raw.githubusercontent.com/sujaanbhalla/grrass-dsml/main/file1.csv"
+
+df = pd.read_csv(url)
 
 print(df)
 
